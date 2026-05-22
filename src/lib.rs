bf14af65d7898a077a67a1b7f32d5cd53da7a839
CrystalCaveYoshi <louis.goerlt@gmail.com>
2026-05-22 21:34:33 +0200

    modify macro

--- src/lib.rs
+++ src/lib.rs
@@ -13,30 +13,30 @@ pub type Words = usize;
 #[macro_export]
 macro_rules! inst {
     (ref, $op1:expr, $op2:expr) => {
-        Instruction::Reference(Operand::Variable($op1.to_string()), $op2.to_string())
+        crate::Instruction::Reference(crate::Operand::Variable($op1.to_string()), $op2.to_string())
     };
     (rec, $op1:expr, $op2:expr) => {
-        Instruction::Receive(Operand::Variable($op1.to_string()), $op2)
+        crate::Instruction::Receive(Operand::Variable($op1.to_string()), $op2)
     };
     ($opcode:ident, $op1:expr, $op2:expr) => {
-        Instruction::TwoOp(TwoOpOpcode::$opcode, Operand::Variable($op1.to_string()), Operand::Variable($op2.to_string()))
+        crate::Instruction::TwoOp(crate::TwoOpOpcode::$opcode, crate::Operand::Variable($op1.to_string()), crate::Operand::Variable($op2.to_string()))
     };
     ($opcode:ident, $op1:expr, val $op2:expr) => {
-        Instruction::TwoOp(TwoOpOpcode::$opcode, Operand::Variable($op1.to_string()), Operand::Constant($op2))
+        crate::Instruction::TwoOp(crate::TwoOpOpcode::$opcode, crate::Operand::Variable($op1.to_string()), crate::Operand::Constant($op2))
     };
     ($opcode:ident, val $op1:expr, val $op2:expr) => {
-        Instruction::TwoOp(TwoOpOpcode::$opcode, Operand::Constant($op1), Operand::Constant($op2))
+        crate::Instruction::TwoOp(crate::TwoOpOpcode::$opcode, crate::Operand::Constant($op1), crate::Operand::Constant($op2))
     };
     ($opcode:ident, val $op1:expr, $op2:expr) => {
-        Instruction::TwoOp(TwoOpOpcode::$opcode, Operand::Constant($op1), Operand::Variable($op2.to_string()))
+        crate::Instruction::TwoOp(crate::TwoOpOpcode::$opcode, crate::Operand::Constant($op1), crate::Operand::Variable($op2.to_string()))
     };
     ($opcode:ident, $op:expr) => {
-        Instruction::SingleOp(SingleOpOpcode::$opcode, Operand::Variable($op.to_string()))
+        crate::Instruction::SingleOp(crate::SingleOpOpcode::$opcode, crate::Operand::Variable($op.to_string()))
     };
     ($opcode:ident, val $op:expr) => {
-        Instruction::SingleOp(SingleOpOpcode::$opcode, Operand::Constant($op))
+        crate::Instruction::SingleOp(crate::SingleOpOpcode::$opcode, crate::Operand::Constant($op))
     };
     ($opcode:ident) => {
-        Instruction::NoOp(NoOpOpcode::$opcode)
+        crate::Instruction::NoOp(crate::NoOpOpcode::$opcode)
     };
 }
