b044c40a005b2ac29a6b06d59cc944486ed16189
CrystalCaveYoshi <louis.goerlt@gmail.com>
2026-05-23 14:30:50 +0200

    Revert "remove nesting"
    This reverts commit 95863908d6e4176eca31290cb15a7357d1273f79.

--- src/instruction.rs
+++ src/instruction.rs
@@ -3,10 +3,9 @@ use crate::{NoOpOpcode, Operand, SingleOpOpcode, TwoOpOpcode, Words};
 #[derive(Debug, Clone)]
 pub enum Instruction {
     Sublabel(String),
-    Alloc(String, Words),
-    Param(String, Words),
-    Result(String, Words),
-    Dealloc,
+    Alloc(String, Words, Vec<Instruction>),
+    Param(String, Words, Vec<Instruction>),
+    Result(String, Words, Vec<Instruction>),
     /// Used to take references. Stores memory location of variable in operand.
     Reference(Operand, String),
     /// Stores nth result of called function in operand.
